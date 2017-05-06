9a4c742af05b5b84d7b0a16ad5cdaa16d29eade6
vo iu <vietrogalaxy@gmail.com>
2017-05-07 01:14:14 +0700

    Initial Commit

---
+++ GIT_led/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIT_led
+//
+//  Created by Shiube on 5/7/17.
+//  Copyright © 2017 ThangNguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GIT_led/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GIT_led
+//
+//  Created by Shiube on 5/7/17.
+//  Copyright © 2017 ThangNguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
